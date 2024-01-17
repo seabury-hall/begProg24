b8489e425b064cb72d07b4fd9b4c8674526e6ffa
Bill Levien <blevien@users.noreply.github.com>
2024-01-17 16:30:47 +0000

    Updates to match lecture

--- Unit0/float_warmup.py
+++ Unit0/float_warmup.py
@@ -1,18 +1,30 @@
-# Get the user's input and convert the input string to a float
-x = input("What's x? ")
-y = input("What's y? ")
+# Get the user's input and convert the input strings to ints
+x = input("Enter a whole number: ")
+y = input("Enter another whole number: ")
 
-# Modify below to add the two inputs, and store the rounded result as z
+# Make sure addition works as we expect it to
 z = x + y
 
 # Print the result
 print(z)
 
-# Modify below to print the formatted result with commas
+# Modify below to print the formatted result with commas (if over 999)
 print(f"{z}")
 
-# Calculate the result of dividing the two numbers and store in variable q
-q = ''
 
-# Modify below to print the formatted result to two decimal places
-print(f"{q}")
+
+# Get the user's input and cast (convert) the input strings to floats
+numerator = input("Enter a decimal number: ")
+denominator = input("Enter another decimal number: ")
+
+# Calculate the result of dividing the two numbers and store in variable quotient
+quotient = ''
+
+# Print the result
+print(f"Result: {quotient}")
+
+# Modify below to use the round function to print the result rounded to two places
+print(f"Result Rounded: {quotient}")
+
+# Modify below to use the formatter to print result with only two decimal places
+print(f"Result to two decimals: {quotient}")
