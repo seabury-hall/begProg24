1f68d3c4fc57ff57a3b26545489a58025468e843
Bill Levien <blevien@users.noreply.github.com>
2024-01-17 16:14:41 +0000

    Unit 0 Warmup

---
+++ Unit0/float_warmup.py
@@ -0,0 +1,18 @@
+# Get the user's input and convert the input string to a float
+x = input("What's x? ")
+y = input("What's y? ")
+
+# Modify below to add the two inputs, and store the rounded result as z
+z = x + y
+
+# Print the result
+print(z)
+
+# Modify below to print the formatted result with commas
+print(f"{z}")
+
+# Calculate the result of dividing the two numbers and store in variable q
+q = ''
+
+# Modify below to print the formatted result to two decimal places
+print(f"{q}")
---
+++ Unit0/string_warmup.py
@@ -0,0 +1,23 @@
+# Ask the user for their name
+name = input("What's your name? ")
+
+# Modify name by using a string function to remove whitespace from the str
+# Store the result in name1
+name1 = name
+
+# Print the output
+print(f"hello, {name1}")
+
+# Modify name by using a string function to Capitalize the first letter of each word
+# Store the result in name2
+name2 = name
+
+# Print the output
+print(f"hello, {name2}")
+
+# Modify name by chaining 2 string functions to Remove whitespace and capitalize the first letter of each word
+# Store the result in name1
+name3 = name
+
+# Print the output
+print(f"hello, {name3}")
